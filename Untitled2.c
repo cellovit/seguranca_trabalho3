0120c8d5a1c82d826da6662a33cdb2a3e8b08f14
cellovit <marcellov1t@hotmail.com>
2017-11-04 21:02:55 -0200

    5

--- Untitled2.c
+++ Untitled2.c
@@ -17,8 +17,8 @@ unsigned int DJBHash(unsigned char* str) {
 int main()
 {
 		struct user{
-    	char* nome;
-    	int senha;
+    	char nome[50];
+    	char senha[50];
 	};
 
 	char qtdUsuarios[10];	
@@ -39,13 +39,71 @@ int main()
     	
     	char *ch;
   		ch = strtok(str1, " ");
-  		usuarios[i].nome = ch;
-  		printf("nome : %s ", usuarios[i].nome);
-  		ch = strtok(NULL, " ");
+  		strcpy(usuarios[i].nome, ch);
   		
-  		usuarios[i].senha = DJBHash(ch);
-  		printf("senha : %d\n", usuarios[i].senha);  	
+//  		usuarios[i].nome = ch;
+  		printf("nome : %s ", usuarios[i].nome);
+  		ch = strtok(NULL, " ,");
+//  		usuarios[i].senha = ch;
+		strcpy(usuarios[i].senha, ch);
+  		printf("senha : %s", usuarios[i].senha);
+  		
+  		
+//  		int senhaEncriptada = DJBHash(usuarios[i].senha);
+//  		
+//  		printf("senha encriptada : %d\n", senhaEncriptada);
 	}
+	
+	for (i = 0; i < qtd; i++){
+		printf("\ntestando\n");
+		printf("nome : %s", usuarios[i].nome);
+		printf("\n");
+		printf("senha : %s", usuarios[i].senha);
+	
+	}
+	
+//	char n[10];
+//	
+//	printf("insira o numero de tentativas : ");
+//	fgets(n, 10, stdin); 
+//	
+//	int nTentativas = atoi(n);
+//	
+//	for (i = 0; i < nTentativas; i++){
+//		
+//		char str2[100];
+//		
+//		printf("Insira o login e a senha :\n");
+//		
+//		fgets(str2, 100, stdin);
+//    	
+//    	char *ch1;
+//  		ch1 = strtok(str2, " ");
+//  		printf("ch : %s\n", ch1);
+//  		
+//  		//verificação de nome
+//  		
+//  		int j;
+//  		int usuarioEncontrado = 0;
+//  		
+//  		for (j = 0; j < qtd; j++){
+//  			printf("usuarios.nome : %s\n", usuarios[j].nome);
+//  			
+//  			if(usuarios[j].nome == ch1){
+//  				usuarioEncontrado = 1;
+//			}
+//		}
+//		
+//		if (usuarioEncontrado == 0){
+//			printf("usuario não encontrado");
+//		}
+//  		
+//  		
+//  		//verificação de senha
+////  		ch = strtok(NULL, " ");
+////  		usuarios[i].senha = ch;
+////  		printf("senha : %s", usuarios[i].senha);
+//	}
     
     return 0;
 }
