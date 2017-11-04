61db819a452617a538eb8648e550d8a657f8ce15
cellovit <marcellov1t@hotmail.com>
2017-11-04 18:12:52 -0200

    2

--- Untitled2.c
+++ Untitled2.c
@@ -3,11 +3,12 @@
 int main()
 {
 		struct user{
-    	char nome[20];
-    	char senha[6];
+    	char* nome;
+    	char* senha;
 	};
 
 	char qtdUsuarios[10];	
+	int i = 0;
 	
     printf("Insira a quantidade de usuarios : ");
 	fgets(qtdUsuarios, 10, stdin); 
@@ -16,9 +17,20 @@ int main()
 	
 	struct user usuarios[qtd]; 
 	
-    char name[100];
-    printf("\nEnter your name:\n ");
-    fgets(name, 100, stdin); 
-    printf("Your Name is: %s", name);
+	printf("\nInsira o nome e a senha dos usuarios:\n ");
+	
+	for (i = 0; i < qtd; i++){
+		char str1[100];    
+    	fgets(str1, 100, stdin);
+    	
+    	char *ch;
+  		ch = strtok(str1, " ");
+  		usuarios[i].nome = ch;
+  		printf("nome : %s", usuarios[i].nome);
+  		ch = strtok(NULL, " ");
+  		usuarios[i].senha = ch;
+  		printf("senha : %s", usuarios[i].senha);
+	}
+    
     return 0;
 }
